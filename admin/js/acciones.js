87a3bb6a0d0c5d3075bb63a2326e2ec5060cadcd
Emanyel <emmanuel.schz.hdz@gmail.com>
2019-09-26 19:11:36 -0500

    Eventos 2.0

---
+++ admin/js/acciones.js
@@ -0,0 +1,23 @@
+function eliminar(){
+ document.getElementById("tarjeta-wrap").innerHTML = "<div> <button onclick='deshacer();' class='btn btn-success'> Deshacer</button></div>";   
+}
+
+function editar(){
+
+}
+
+function guardar(){
+
+}
+
+function cancelar(){
+
+}
+
+function deshacer(){
+    
+}
+
+function contador(){
+
+}
