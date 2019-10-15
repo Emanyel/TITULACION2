b36542b8bda0663aa0795fe7581de653b6614e58
Emmanuel <38230732+Emanyel@users.noreply.github.com>
2019-10-15 17:20:43 -0500

    EDICION TARJETA

--- admin/js/acciones.js
+++ admin/js/acciones.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
 	$(".button1").click(function(){
-		
+		window.open('../html/edicion.php', '_blank');
 	});
 
 	$(".button2").click(function(){
+		Swal.fire({
+			type:'warning',
+			title: 'Estas seguro?',
+			text: 'Si eliminas un evento no podras deshacer esta acción.',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			cancelButtonText: 'Cancelar',
+			confirmButtonText: 'Si, hazlo!'
+		  }).then((result) => {
+			if (result.value) {
+				borrar();
+			  Swal.fire(
+				'Borrado!',
+				'El evento ha sido borrado',
+				'success'
+			  )
+			}
+		})
 	});
 
 	$("#mas").click(function(){
@@ -12,6 +31,14 @@ $(document).ready(function(){
 
 	$("#menos").click(function(){
 
+	});
+
+	function borrar(){
+		$('')
+	}
+
+	$("nuevoEvento").click(function(){
+		
 	});
 
 });
