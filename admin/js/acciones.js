2c0d9021ae5d09ea7e7b63e33511157a6b21177c
Emmanuel <38230732+Emanyel@users.noreply.github.com>
2019-10-16 14:25:37 -0500

    AGREGAR EVENTOS BOTON

--- admin/js/acciones.js
+++ admin/js/acciones.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-	$(".button1").click(function(){
-		window.open('../html/edicion.php', '_blank');
-	});
+	// CONTAMOS CUANTOS ITEM EXISTEN (TARJETAS)
+	var items =  document.getElementsByClassName("item").length;
+	console.log(items);
 
-	$(".button2").click(function(){
+
+		//ACCCION PARA EL BOTON EDITAR
+	$(document).on('click', '.button1', function(){
+		window.open('../html/edicion.html', '_blank');
+	});
+		//ACCCION PARA EL BOTON ELIMINAR
+	$(document).on('click', '.button2', function(){
 		Swal.fire({
 			type:'warning',
 			title: 'Estas seguro?',
@@ -15,7 +21,7 @@ $(document).ready(function(){
 			confirmButtonText: 'Si, hazlo!'
 		  }).then((result) => {
 			if (result.value) {
-				borrar();
+				borrarEvento();
 			  Swal.fire(
 				'Borrado!',
 				'El evento ha sido borrado',
@@ -24,21 +30,54 @@ $(document).ready(function(){
 			}
 		})
 	});
-
-	$("#mas").click(function(){
-		
+		//ACCION PARA EL BOTON CREAR UN NUEVO EVENTO
+	$(document).on('click', '.newEvent', function(){
+			
+				$('.nuevoEvento').replaceWith('<div class="entradas">'+
+				'<h4>Parte delantera de la tarjeta</h4>'+
+				'<form id="form" action="agregarEvento.php" method="GET">'+
+				'<div class="form-group"><input placeholder="Escribe el nombre del evento" required/></div>'+
+				'<div class="form-group"><input placeholder="Escribe la dirección" required/></div>'+
+				'<div class="form-group"><input type="date" placeholder="Selecciona la fecha" required/></div>'+
+				'<button type="submit" class="aceptar" id="boton">Aceptar</button>'+
+				'</form>'+
+				'<button class="cancelar" id="boton">Cancelar</button>'+
+				'</div>');
+	});
+		//ACCION PARA ACEPTAR LA INFO DEL NUEVO EVENTO
+	$(document).on('click', '.aceptar', function(){
+		 
+	});
+		//ACCION PARA DECLINAR LA INFO DEL NUEVO EVENTO
+	$(document).on('click', '.cancelar', function(){
+		Swal.fire({
+			type:'warning',
+			title: 'Estas seguro?',
+			text: 'No se guardará ningun dato ingresado',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			cancelButtonText: 'Cancelar',
+			confirmButtonText: 'Si, hazlo!'
+		  }).then((result) => {
+			if (result.value) {
+				agregarButtonEvento();
+			  Swal.fire(
+				'Hecho!',
+				'Aqui no pasó nada',
+				'success'
+			  )
+			}
+		})
 	});
 
-	$("#menos").click(function(){
-
-	});
-
-	function borrar(){
-		$('')
+	//FUNCIONES DE AYUDA
+	function agregarButtonEvento(){
+		$(".entradas").replaceWith("<div class='nuevoEvento'><button id='boton' class='newEvent'>Agregar evento</button></div>");
 	}
 
-	$("nuevoEvento").click(function(){
+	function borrarEvento(){
 		
-	});
+	}
 
 });
